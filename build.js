ab3865e5c313c58da65f19da8318f0ce26506a2a
Miguel Castillo <manchagnu@gmail.com>
2014-11-13 17:18:11 -0500

    Adding test files

---
+++ build.js
@@ -0,0 +1,11 @@
+({
+  "optimize": "none",
+  "name": "index",
+  "out": "index-built.js",
+  "paths": {
+    "css": "node_modules/require-css/css.min",
+    "css-builder": "node_modules/require-css/css-builder",
+    "normalize": "node_modules/require-css/normalize"
+  },
+  "writeCSSModule": true
+})
